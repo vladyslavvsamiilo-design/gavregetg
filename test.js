5ff31d8158d5f8464bf4da806f1a34c56a79f6dd
vladyslavvsamiilo-design <vladyslav.v.samiilo@ukd.edu.ua>
2025-12-05 06:57:55 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,31 @@
+let n1 = []
+for (let i = 0; i < 20; i++)
+{
+    n1[i] = Math.round(Math.random() * 100 - 50);
+}
+
+function findMaxInArray(inputArray = []) {
+    let max = inputArray[0]
+
+    if (inputArray.length === 0) {
+        return null;
+    }
+
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] > max) {
+            max = inputArray[i]
+        }
+    }
+
+    return max;
+}
+
+function displayPrice(price) {
+    const normalizedPrice = price / 100;
+    console.log(`${normalizedPrice} in USD`);
+}
+
+const maxFromArray = findMaxInArray();
+console.log(maxFromArray);
+
+displayPrice(99);

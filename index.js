022d06e537bf67ff4f3452f9d75cb66e6cbc12b1
vladyslavvsamiilo-design <vladyslav.v.samiilo@ukd.edu.ua>
2025-12-05 06:57:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+let number_num = 5;
+let some_new = "yes";
+console.log(some_new + number_num + "!");
+number_num = 7;
+console.log(number_num);
+
+const number = 5;
+// number = 3;
+console.log(number);
+
+let isHasCar = false;
+number_num = "ha";
+console.log(number_num);
+let num1 = 15;
+let num2 = 7;
+//let res = num1 - num2;
+console.log("Result: " + (num1 - num2));
+console.log("Result: " + (num1 + num2));
+console.log("Result: " + (num1 * num2));
+console.log("Result: " + (num1 / num2));
+console.log("Result: " + (num1 % num2));
+
+let num3 = 5;
+let num4 = Number("10");
+console.log(num3+num4);
+let num5 = 6;
+let num6 = "11";
+console.log(num5+parseInt(num6,10));
+
+num1 += 10;
+console.log(num1)
+
+let p1 = Math.PI
+console.log(Math.max(1,9,-100,4));
+let number456 = 15;
+
+if (number456 >= 15) {
+    console.log("Yes"); 
+}  
+ let number457 = 15;
+ let MyHasCar = true;
+
+if (number457 >= 15 || MyHasCar) {
+ console.log("Yes"); 
+    console.log("Yes, me has car");
+}
+else if(number457 == 15){
+console.log("Num is 15")
+}
+else if(number457 === 10){
+console.log("Num is 15")
+}
+else if(number457 != 1){
+console.log("Num is 15")
+}
+else {
+    console.log("No")
+}
+let word = 'Some';
+
+switch(word) {
+    case 'new': console.log("Let is new"); break;
+}
+let data = [4, 3, 6, 5.4, 'Word', true, [true, 5]];//new Array(4, 5, 2);
+data[1] = 'Some';
+console.log(data[1]);
+
+let matrix = [
+    [5, 6], 
+    ["World", false], 
+    [true, 5.9, 0]
+];
+matrix[0] [0] = 60;
+console.log(matrix[2] [1]);
+
+number = 6;
